6a0e4c36e32a013008d3f9f14c8654100fbeb2e2
Jana Maskova <jana.maskova93@gmail.com>
2022-03-22 20:19:06 +0100

    Cviceni na hodine for/while

--- main.js
+++ main.js
@@ -1,31 +1,72 @@
 // Použij výpis do konzole prohlížeče pomocí console.log('text')
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
-
-// První příklad - vypiš vzestupně čísla od 0 do 10
-console.log('První příklad - vzestupná řada 0 - 10');
-
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
+// *************************
+//     WHILE
+// ****************************
 
 
+// První příklad - vypiš vzestupně čísla od 0 do 10 přes while
 
-// Druhý příklad - zkopíruj předchozí příklad
+console.log('První příklad - vzestupná řada 0 - 10 WHILE' );
+
+let i=0
+while(i<10){
+    i=i+1
+    console.log(i)
+}
+
+
+
 // a vyzkoušej, jaký je rozdíl mezi <= a <
-// v podmínce uvnitř cyklu
+// v podmínce uvnitř cyklu přes while
 
-console.log('Druhý příklad - rozdíl mezi <= a <');
+console.log('Druhý příklad - vzestupná řada 0 - 10 WHILE' );
 
-//TADY NAPIS CYKLUS
-
-console.log('-------------------');
-
+let a=0
+while(a<=10){
+    a=a+1
+    console.log(a)
+}
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
-console.log('Třetí příklad - sestupná řada 10 - 0');
+// console.log('Třetí příklad - sestupná řada 10 - 0');
 
-//TADY NAPIS CYKLUS
+console.log('Třetí příklad - sestupná řada 0 - 10 WHILE' );
 
-console.log('-------------------');
+let b=11
+while (b>0){
+    b=b-1
+    console.log (b)
+}
+
+
+
+
+
+// *************************
+//     for
+// ****************************
+
+// // První příklad - vypiš vzestupně čísla od 0 do 10
+// console.log('První příklad - vzestupná řada 0 - 10');
+// for(i=0; i<=10; i=i+1){
+//     console.log(i)
+// }
+
+
+// // Druhý příklad - zkopíruj předchozí příklad
+// // a vyzkoušej, jaký je rozdíl mezi <= a <
+// // v podmínce uvnitř cyklu
+// for(i=0; i<10; i=i+1){
+//     console.log(i)
+// }
+
+
+// // Třetí příklad - vypiš sestupně čísla od 10 do 0
+// // console.log('Třetí příklad - sestupná řada 10 - 0');
+
+// for (i=10; i>=0; i=i-1){
+//     console.log (i)
+// }
